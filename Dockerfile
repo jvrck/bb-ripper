633924a9ad5adfffec5ad06cf236acb07288b8bc
Jim Vrckovski <100883+jvrck@users.noreply.github.com>
2023-10-10 15:07:13 +1100

    1.0.4 (#134)
    * Fix changelog with 1.0.3 details. (#128)
    
    * Upgrade to AWS CLI v2. (#130)
    
    * Bump urllib3 from 2.0.5 to 2.0.6 (#131)
    
    Bumps [urllib3](https://github.com/urllib3/urllib3) from 2.0.5 to 2.0.6.
    - [Release notes](https://github.com/urllib3/urllib3/releases)
    - [Changelog](https://github.com/urllib3/urllib3/blob/main/CHANGES.rst)
    - [Commits](https://github.com/urllib3/urllib3/compare/v2.0.5...2.0.6)
    
    ---
    updated-dependencies:
    - dependency-name: urllib3
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Add Docker image test workflow (#132)
    
    Adds a github actions workflow that builds and tests Docker images on pull requests and release branches.
    
    * Update push-test-image.yml (#133)
    
    Expand branch pattern
    
    * Update CHANGELOG.md
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- Dockerfile
+++ Dockerfile
@@ -7,11 +7,9 @@ COPY requirements.txt /ripper
 COPY bb-ripper/ /ripper/
 COPY setup.sh /ripper
 
-RUN chmod +x setup.sh
-RUN /ripper/setup.sh
-RUN rm /ripper/setup.sh
 RUN apt-get update && apt-get upgrade -y && apt-get install -y git
-RUN  pip install -r requirements.txt
+RUN chmod +x setup.sh && ./setup.sh && rm setup.sh
+RUN pip install -r requirements.txt
 
 VOLUME /data
 
--- setup.sh
+++ setup.sh
@@ -3,7 +3,18 @@ set -ex
 
 # Install AWS CLI 
 if [ "$AWSCLI" == "TRUE" ] ; then 
-    pip3 install awscli ; 
+    # Required for AWS CLI download and unzip. Remove after installation.
+    apt-get install -y curl unzip ;
+    
+    # Download and install AWS CLI. Clean up after installation.
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" ;
+    unzip awscliv2.zip ;
+    ./aws/install ;
+    rm -rf aws ;
+    rm -rf awscliv2.zip ;
+
+    # Clean up after installation.  
+    apt-get -y --purge remove curl unzip ;
 else 
     echo Argument not provided ; 
 fi
